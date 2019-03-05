9c5af5965f15babbb2c07942c08b000511509525
Riley Gibbs <rileyjohngibbs@gmail.com>
2019-03-05 08:56:45 -0800

    Added multiple-pools feature
    TODO:
    - Style Pool display more compactly; more friendly display of multiple
    pools on the screen.
    - Style add and remove buttons.
    - Add button to roll all pools.
      - Probably requires lifting state.
    - Fix the bug with the modifiers not applying.

--- src/App.js
+++ src/App.js
@@ -1,126 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import DieComp from './DieComp';
+import Pool from './Pool';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dice: [],
-      mod: 0
+      pools: [0]
     };
   }
 
-  addDie(faces) {
-    const dice = this.state.dice;
-    dice.push({faces: faces, value: 0});
-    this.setState({dice});
+  addPool() {
+    const pools = this.state.pools;
+    pools.push(Math.max(-1, ...pools) + 1);
+    this.setState({pools});
   }
 
-  removeDie(key) {
-    const dice = this.state.dice;
-    dice.splice(key, 1);
-    this.setState({dice});
+  removePool(key) {
+    this.setState({pools: this.state.pools.filter(p => p !== key)});
   }
 
-  rollDie(key) {
-    const dice = this.state.dice;
-    dice[key].value = Math.ceil(Math.random() * dice[key].faces);
-    this.setState({dice});
-  }
-
-  renderDesc() {
-    const counts = {};
-    this.state.dice.forEach((die) => {
-      counts[die.faces] = counts[die.faces] === undefined
-        ? 1
-        : counts[die.faces] + 1;
-    });
-    const pool = Object.keys(counts).map(
-      (type) => counts[type] + "d" + type
-    ).join(" + ");
-    const mod = this.state.mod
-      ? this.state.mod > 0
-        ? " + " + this.state.mod
-        : " - " + (-this.state.mod)
-      : "";
-    return pool + mod;
-  }
-
-  calcSum() {
-    return this.state.dice.reduce((acc, die) => acc + die.value, 0);
-  }
-
-  render() {
-    const dice = this.state.dice.map((die, key) => {
-      return <DieComp key={key} className="die col-2"
-        faces={die.faces}
-        value={die.value}
-        rollBack={(newValue) => {
-          const dice = this.state.dice;
-          dice[key].value = newValue;
-          this.setState({dice});
-        }}
-        tearDown={() => this.removeDie(key)}
-      />
-    });
+  render () {
     return (
-      <div className="App container">
-        <div id="adders" className="row">
-          {[4, 6, 8, 10, 100, 12, 20].map((faces, key) => {
-            return (
-              <button className="col" key={key} onClick={() => {this.addDie(faces);}}>
-                +d{faces}
+      <div className="App">
+        <button onClick={() => this.addPool()}>Add Pool</button>
+        {this.state.pools.map((key) => {
+          return (
+            <div key={key}>
+              <button onClick={() => this.removePool(key)}>
+                Remove
               </button>
-            )
-          })}
-        </div>
-        <div id="controllers">
-          <div id="total-display" className="row">
-            <div className="col-8" style={{padding: 0}}>
-              <button style={{float: "left"}} onClick={() => {this.setState({mod: this.state.mod - 1})}}>
-                -
-              </button>
-              {this.renderDesc()}
-              <button style={{float: "right"}} onClick={() => {this.setState({mod: this.state.mod + 1})}}>
-                +
-              </button>
+              <Pool />
             </div>
-            <div className="col-4">
-              Total: {this.calcSum()}
-            </div>
-          </div>
-          <div className="row">
-            <button className="col" onClick={() => {
-                this.state.dice.forEach((_, key) => {
-                  this.rollDie(key);
-                });
-              }}>
-              Roll Dice
-            </button>
-            <button className="col" onClick={() => {
-                while (this.state.dice.length) {
-                  this.removeDie(0);
-                }
-                this.setState({mod: 0});
-              }}>
-              Clear Pool
-            </button>
-            <button className="col" onClick={() => {
-                const dice = this.state.dice;
-                dice.forEach((d) => {d.value = 0});
-                this.setState({dice});
-              }}>
-              Reset Dice
-            </button>
-          </div>
-        </div>
-        <div id="dicepool" className="row">
-          {dice}
-        </div>
+          )
+        })}
       </div>
-    );
+    )
   }
 }
 
