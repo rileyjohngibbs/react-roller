55bffaea0263a3993da4bcc682c4336a0aa1a515
Alberto101Ma <48220264+Alberto101Ma@users.noreply.github.com>
2019-03-07 06:07:19 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 
 import Die from './Die';
 
+function counter123(){
 let counter = 0;
-
    if(counter>11){
     const buttons = document.getElementById("disabled");
     buttons.disabled = true;
