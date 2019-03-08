a87c29e1b18eb379ad751e212df733bd955a1ca1
Riley Gibbs <rileyjohngibbs@gmail.com>
2019-03-07 20:48:29 -0800

    Cleaned up DieComp and click handlers

--- src/App.js
+++ src/App.js
@@ -13,16 +13,20 @@ class App extends Component {
     };
   }
 
-  addDie(faces) {
-    const dice = this.state.dice;
-    dice.push({faces: faces, value: 0});
-    this.setState({dice});
+  dieAdder(faces) {
+    return () => {
+      const dice = this.state.dice;
+      dice.push({faces: faces, value: 0});
+      this.setState({dice});
+    }
   }
 
-  removeDie(key) {
-    const dice = this.state.dice;
-    dice.splice(key, 1);
-    this.setState({dice});
+  dieRemover(key) {
+    return () => {
+      const dice = this.state.dice;
+      dice.splice(key, 1);
+      this.setState({dice});
+    }
   }
 
   rollDie(key) {
@@ -35,17 +39,26 @@ class App extends Component {
     return this.state.dice.reduce((acc, die) => acc + die.value, 0);
   }
 
+  rollBack(key) {
+    return (newValue) => {
+      const dice = this.state.dice;
+      dice[key].value = newValue;
+      this.setState({dice});
+    }
+  }
+
+  modTicker(inc) {
+    return () => {
+      this.setState({mod: this.state.mod + inc});
+    }
+  }
+
   render() {
     const dice = this.state.dice.map((die, key) => {
       return <DieComp key={key} className="die col-2"
-        faces={die.faces}
-        value={die.value}
-        rollBack={(newValue) => {
-          const dice = this.state.dice;
-          dice[key].value = newValue;
-          this.setState({dice});
-        }}
-        tearDown={() => this.removeDie(key)}
+        die={die}
+        rollBack={this.rollBack(key)}
+        tearDown={this.dieRemover(key)}
       />
     });
     return (
@@ -53,7 +66,7 @@ class App extends Component {
         <div id="adders" className="row">
           {[4, 6, 8, 10, 100, 12, 20].map((faces, key) => {
             return (
-              <button className="col" key={key} onClick={() => {this.addDie(faces);}}>
+              <button className="col" key={key} onClick={this.dieAdder(faces)}>
                 +d{faces}
               </button>
             )
@@ -62,11 +75,11 @@ class App extends Component {
         <div id="controllers">
           <div id="total-display" className="row">
             <div className="col-8" style={{padding: 0}}>
-              <button style={{float: "left"}} onClick={() => {this.setState({mod: this.state.mod - 1})}}>
+              <button style={{float: "left"}} onClick={this.modTicker(-1)}>
                 -
               </button>
               <PoolDescription dice={this.state.dice} mod={this.state.mod} />
-              <button style={{float: "right"}} onClick={() => {this.setState({mod: this.state.mod + 1})}}>
+              <button style={{float: "right"}} onClick={this.modTicker(1)}>
                 +
               </button>
             </div>
@@ -84,7 +97,7 @@ class App extends Component {
             </button>
             <button className="col" onClick={() => {
                 while (this.state.dice.length) {
-                  this.removeDie(0);
+                  this.dieRemover(0)();
                 }
                 this.setState({mod: 0});
               }}>
--- src/DieComp.js
+++ src/DieComp.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 
 class DieComp extends Component {
+  constructor(props) {
+    super(props)
+    this.roll = this.roll.bind(this);
+  }
+
   roll() {
-    this.props.rollBack(Math.ceil(Math.random() * this.props.faces));
+    this.props.rollBack(Math.ceil(Math.random() * this.props.die.faces));
   }
 
   render() {
     return (
       <div className={this.props.className}>
-        <button onClick={() => this.roll()}>
-          {this.props.value}
+        <button onClick={this.roll}>
+          {this.props.die.value}
         </button>
         <div className="die-label">
-          d{this.props.faces}
+          d{this.props.die.faces}
         </div>
         <div className="die-remove">
-          <button onClick={() => this.props.tearDown()}>
+          <button onClick={this.props.tearDown}>
             Drop
           </button>
         </div>
